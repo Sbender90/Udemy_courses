86e293721439ef4089e2ecac737f14aa40772c73
Steven Bender <sbender1990@gmail.com>
2018-04-27 23:54:23 -0500

    Lesson 2 variables

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -1,15 +1,42 @@
+// Lecture: variables
+/*
 var name = "John";
-
 console.log(name);
 
 var lastName = "Smith";
-
 console.log(lastName);
 
 var age = 28;
-
 console.log(age);
 
 var fullAge = true;
-
 console.log(fullAge);
+*/
+
+// Lecture: variables 2
+
+var name = "Steven";
+var age = 27;
+
+// console.log(name + " " + age);
+// console.log(age + age);
+
+var job, isMarried;
+// console.log(job);
+
+job = "Programmer";
+isMarried = true;
+
+console.log(name + " is a " + age + " year old " + job + ", is he married? " + isMarried + ".");
+
+name = "John";
+age = "Twenty Eight";
+job = "Coder";
+isMarried = false;
+
+console.log(name + " is a " + age + " year old " + job + ", is he married? " + isMarried + ".");
+
+//var lastName = prompt("What is the last name?");
+//console.log(lastName);
+
+alert(name + " is a " + age + " year old " + job + ", is he married? " + isMarried + ".")
